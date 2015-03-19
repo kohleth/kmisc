17d6e3892dc661ff0c906be552ea0e8a45dceb36
Kohleth <kohleth@gmail.com>
2015-03-19 12:55:19 +1100

    Recoded fitAll to work with object instead of formula. Removed fitAll.default.

--- R/fit_all.R
+++ R/fit_all.R
@@ -23,88 +23,106 @@
 #' @export
 fitAll <- function(x, data, method, ...) UseMethod("fitAll")
 
+
 #' @export
 #' @rdname fitAll 
-fitAll.default <- function(x, data, method = "lm", description = "fitting", progress = TRUE, 
+fitAll.lm <- function(x, data, description = "fitting", progress = TRUE, 
     ...) {
-    form = .getAllForm(x, data,method=method,...)
+    form = formula(x)
+    method = class(x)
+    forms = getAllForm(form, method = method, ...)
     if (progress == TRUE) 
-        message(description, " ", length(form), " models...")
+        message(description, " ", length(forms), " models...")
     method <- as.name(method)
     fitmodel <- function(f) {
         eval(substitute(method(f, data = data, ...), list(f = f, method = method)))
     }
-    models <- plyr::llply(form, fitmodel, .progress = ifelse(progress == TRUE, "text", 
-        "none"))
+    models <- plyr::llply(forms, fitmodel, .progress = ifelse(progress == 
+        TRUE, "text", "none"))
     names(models) <- seq_along(models)
-    structure(models, data = data, class = "ensemble")
+    structure(models, class = "ensemble")
 }
 
 #' @export
 #' @rdname fitAll 
-fitAll.lm <- function(x, data, method = "lm", ...) {
-    x = formula(x)
-    fitAll.default(x, data, method, ...)
+fitAll.lme <- function(x, data, method = "lme", description = "fitting", 
+    progress = TRUE, ...) {
+    fixed = formula(x)
+    random = as.formula(x$call$random)
+    method = class(x)
+    forms = getAllForm(fixed, method = method, ...)
+    if (progress == TRUE) 
+        message(description, " ", length(forms), " models...")
+    method <- as.name(method)
+    fitmodel <- function(f) {
+        eval(substitute(method(f, data = data, random = random, ...), list(f = f, 
+            method = method)))
+    }
+    models <- plyr::llply(forms, fitmodel, .progress = ifelse(progress == 
+        TRUE, "text", "none"))
+    names(models) <- seq_along(models)
+    structure(models, class = "ensemble")
 }
+
 
 #' @export
 #' @rdname fitAll 
-fitAll.lme <- function(x,data,method = "lme",...){
-  fixed=formula(x)
-  random=as.formula(x$call$random)
-  fitAll.default(x=fixed,data,method,random=random)
+fitAll.merMod <- function(x, data, description = "fitting", progress = TRUE, 
+    ...) {
+    fixed = nobars(formula(x))
+    random = paste0("(", .fb2(formula(x)), ")")
+    method = "lmer"
+    forms = getAllForm(fixed, method = method, random = random, ...)
+    if (progress == TRUE) 
+        message(description, " ", length(forms), " models...")
+    method <- as.name(method)
+    fitmodel <- function(f) {
+        eval(substitute(method(f, data = data, ...), list(f = f, method = method)))
+    }
+    models <- plyr::llply(forms, fitmodel, .progress = ifelse(progress == 
+        TRUE, "text", "none"))
+    names(models) <- seq_along(models)
+    structure(models, class = "ensemble")
+}
+
+.fb2 <- function(term) {
+    if (is.name(term) || !is.language(term)) 
+        return(NULL)
+    if (term[[1]] == as.name("(")) 
+        return(.fb2(term[[2]]))
+    stopifnot(is.call(term))
+    if (term[[1]] == as.name("|")) 
+        return(term)
+    if (length(term) == 2) 
+        return(.fb2(term[[2]]))
+    c(.fb2(term[[2]]), .fb2(term[[3]]))
 }
 
 
-#' @export
-#' @rdname fitAll 
-fitAll.lmer <- function(x,data,method = "lmer",...){
-  fixed=nobars(formula(x))
-  random=paste0("(",fb2(formula(x)),")")
-  fitAll.default(x=fixed,data,method,random=random)
+
+getAllForm = function(x, method, random = NULL, ...) {
+    combs = getAllFormMat(x)
+    vars <- apply(combs, 1, function(i) colnames(combs)[i])
+    form <- paste(deparse(x[[2]]), "~", lapply(vars, paste, collapse = " + "), 
+        sep = "")
+    form[[1]] <- paste(deparse(x[[2]]), "~1")
+    if (method == "lmer") 
+        form <- paste(form, random, sep = "+")
+    form <- lapply(form, as.formula, env = baseenv())
+    form
 }
 
-.getAllForm = function(x, data, method,random=NULL,...) {
-    tt <- attr(terms(x, data = data), "factors")
+
+getAllFormMat = function(x) {
+    tt <- attr(terms(x), "factors")
     if (ncol(tt) <= 1) 
         stop("formula should be more complicated which allows non-trivial nested models.")
     comb0 <- do.call(expand.grid, rep(list(c(FALSE, TRUE)), ncol(tt)))
     ttf <- tt[-1, ]
-    cond <- rbind(ttf, cbind(matrix(0, nrow = diff(dim(ttf)), ncol = nrow(ttf)), diag(diff(dim(ttf)))))  ## conditions for interaction term to exist
+    cond <- rbind(ttf, cbind(matrix(0, nrow = diff(dim(ttf)), ncol = nrow(ttf)), 
+        diag(diff(dim(ttf)))))
     ncond <- colSums(cond)
-    combs <- unique(t(apply(as.matrix(comb0) %*% cond, 1, function(x) x == ncond)))
-    vars <- apply(combs, 1, function(i) colnames(combs)[i])
-    form <- paste(deparse(x[[2]]), "~", lapply(vars, paste, collapse = " + "), sep = "")
-    form[[1]] <- paste(deparse(x[[2]]), "~1")
-    if(method=="lmer")form<-paste(form,random,sep="+")
-    form <- lapply(form, as.formula, env = baseenv())
-    form
+    combs <- unique(t(apply(as.matrix(comb0) %*% cond, 1, function(x) x == 
+        ncond)))
+    combs
 } 
-
-fb2 <- function(term) {
-  if (is.name(term) || !is.language(term)) 
-    return(NULL)
-  if (term[[1]] == as.name("(")) 
-    return(fb2(term[[2]]))
-  stopifnot(is.call(term))
-  if (term[[1]] == as.name("|")) 
-    return(term)
-  if (length(term) == 2) 
-    return(fb2(term[[2]]))
-  c(fb2(term[[2]]), fb2(term[[3]]))
-}
-
-getAllFormMat=function (x, data) 
-{
-  tt <- attr(terms(x, data = data), "factors")
-  if (ncol(tt) <= 1) 
-    stop("formula should be more complicated which allows non-trivial nested models.")
-  comb0 <- do.call(expand.grid, rep(list(c(FALSE, TRUE)), ncol(tt)))
-  ttf <- tt[-1, ]
-  cond <- rbind(ttf, cbind(matrix(0, nrow = diff(dim(ttf)), 
-                                  ncol = nrow(ttf)), diag(diff(dim(ttf)))))
-  ncond <- colSums(cond)
-  combs <- unique(t(apply(as.matrix(comb0) %*% cond, 1, function(x) x == 
-                            ncond)))
-  combs
-}
